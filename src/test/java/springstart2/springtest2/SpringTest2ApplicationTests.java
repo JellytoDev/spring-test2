247a7777c519785e9498e99ef31771aef8800539
JellytoDev <wjdwhdgus2222@naver.com>
2022-01-12 22:49:45 +0900

    Initial commit

---
+++ src/test/java/springstart2/springtest2/SpringTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springstart2.springtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
